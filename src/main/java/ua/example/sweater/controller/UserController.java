1f7a59c2f962867eb8c6ffca64f890f518eae7fc
Vadym Zakharchenko <vadim.if@gmail.com>
2018-06-26 12:37:27 +0300

    Add userEdit page and access to page by Admin Role

--- src/main/java/ua/example/sweater/controller/UserController.java
+++ src/main/java/ua/example/sweater/controller/UserController.java
@@ -1,14 +1,21 @@
 package ua.example.sweater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ua.example.sweater.domain.Role;
+import ua.example.sweater.domain.User;
 import ua.example.sweater.repository.UserRepository;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('Admin')")
 public class UserController {
 
     @Autowired
@@ -18,6 +25,36 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEdit(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam String username,
+                           @RequestParam Map<String,String> form,
+                           @RequestParam("id") User user) {
+
+        user.setUsername(username);
+        //Convert enum to String
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        //Clear all user roles
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+        return "redirect:/user";
     }
 
 }
