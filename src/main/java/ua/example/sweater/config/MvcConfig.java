2d1eab4069b6fc5bef6c999dfbe4306e6f2f8146
Vadym Zakharchenko <vadim.if@gmail.com>
2018-07-04 16:07:40 +0300

    Add static resources

--- src/main/java/ua/example/sweater/config/MvcConfig.java
+++ src/main/java/ua/example/sweater/config/MvcConfig.java
@@ -21,5 +21,8 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
                 .addResourceLocations("file://" + uploadPath + "/");
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
